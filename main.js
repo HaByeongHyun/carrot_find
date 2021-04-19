c844813dae8548b74669e09cc04e3ad6a8df76ca
the9616 <the9616@naver.com>
2021-04-19 17:37:38 +0900

    JS : Timer

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+let time = 60;
+let min = '';
+let sec = '';
+
+const x = setInterval(() => {
+  min = parseInt(time / 60);
+  sec = time % 60;
+
+  const timer = document.querySelector('.timer');
+  timer.innerHTML = `${min}:${sec}`;
+  time--;
+
+  if (time < 0) {
+    clearInterval(x);
+    document.querySelector('.timer').innerHTML = '시간초과';
+  }
+}, 1000);
