a5d7d4e70b58086b59c10b1dc2bd67bc469a5132
the9616 <the9616@naver.com>
2021-04-20 10:59:14 +0900

    JS : random image, timer

--- main.js
+++ main.js
@@ -4,22 +4,39 @@ let time = 60;
 let min = '';
 let sec = '';
 
-const x = setInterval(() => {
-  min = parseInt(time / 60);
-  sec = time % 60;
+const start = document.querySelector('.start');
+start.addEventListener('click', () => {
+  const x = setInterval(() => {
+    min = parseInt(time / 60);
+    sec = time % 60;
 
-  const timer = document.querySelector('.timer');
-  timer.innerHTML = `${min}:${sec}`;
-  time--;
+    const timer = document.querySelector('.timer');
+    timer.innerHTML = `${min}:${sec}`;
+    time--;
 
-  if (time < 0) {
-    clearInterval(x);
-    document.querySelector('.timer').innerHTML = '시간초과';
+    if (time < 0) {
+      clearInterval(x);
+      document.querySelector('.timer').innerHTML = '시간초과';
+    }
+  }, 1000);
+});
+
+function show_image(src, width, height, alt) {
+  const img = document.createElement('img');
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  img.style.position = 'absolute';
+  img.style.top = document.body.clientHeight * Math.random() + 'px';
+  img.style.left = document.body.clientWidth * Math.random() + 'px';
+  document.body.appendChild(img);
+}
+
+window.onload = () => {
+  for (let i = 0; i < 10; i++) {
+    show_image('/img/bug.png', 80, 80, 'bug');
+    show_image('/img/carrot.png', 80, 80, 'bug');
   }
-}, 1000);
-
-// 이미지 생성
-const body = document.querySelector('body');
-const image = new Image(80, 80);
-image.src = `/img/bug.png`;
-body.appendChild(image);
+};
